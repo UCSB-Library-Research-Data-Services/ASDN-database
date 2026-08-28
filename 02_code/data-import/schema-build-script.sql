ace20c2c585341360e32e15528e1c395acc3e0a2
Julien Brun <brunj7@users.noreply.github.com>
2026-08-28 16:39:01 -0700

    rebuild databse with cleaned observers and predators table

--- 02_code/data-import/schema-build-script.sql
+++ 02_code/data-import/schema-build-script.sql
@@ -63,7 +63,7 @@ CREATE TABLE Bird_nests (
     FOREIGN KEY (Species) REFERENCES Species (Code),
     FOREIGN KEY (Observer) REFERENCES Personnel (Abbreviation)
 );
-COPY Bird_nests FROM '../01_data/data-processed/bird_nests.csv' (header TRUE, NULL "NA");
+COPY Bird_nests FROM '../01_data/data-processed/bird_nests_abbrev.csv' (header TRUE, NULL "NA");
 
 CREATE TABLE Bird_eggs (
     Book_page VARCHAR,
@@ -77,7 +77,7 @@ CREATE TABLE Bird_eggs (
     FOREIGN KEY (Site) REFERENCES Site (Code),
     FOREIGN KEY (Nest_ID) REFERENCES Bird_nests (Nest_ID)
 );
-COPY Bird_eggs FROM '../01_data/data-processed/bird_eggs.csv' (header TRUE);
+COPY Bird_eggs FROM '../01_data/data-processed/bird_eggs_observers_matched.csv' (header TRUE);
 
 
 CREATE TABLE Predators (
